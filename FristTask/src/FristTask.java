2c6fa1ac4da62e7bf5cf39752f4e1020c1ff628b
AndreiTe <43140003+AndreiTe@users.noreply.github.com>
2020-07-06 13:46:54 +0300

    Add files via upload

--- FristTask/src/FristTask.java
+++ FristTask/src/FristTask.java
@@ -7,8 +7,10 @@ public class FristTask {
         int znacenie[] = {0,1,2,3};
         int vesa[] = {1,2,3,4};
         int test[] = new int[4];
+        int q =100000 ;
         Random x = new Random(znacenie, vesa);
-        for (int i = 0; i < 100; i++){
+
+        for (int i = 0; i < q; i++){
             int asd = x.myFunc();
             if (asd == 0) {
                 test[0]+=1;
@@ -25,7 +27,7 @@ public class FristTask {
 
         }
         for (int i = 0; i < 4; i++){
-            System.out.println(test[i]);
+            System.out.println(test[i]*100/q+"%");
         }
        System.out.print(x.myFunc());
          }
